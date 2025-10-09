8d5509faeca3d305764be103a5b67bf5980fd1fb
Leester <128757198+Leescifer@users.noreply.github.com>
2025-10-09 21:20:23 +0800

    update

--- CSV-ticketing/src/pages/payroll/archivePayroll.tsx
+++ CSV-ticketing/src/pages/payroll/archivePayroll.tsx
@@ -1,12 +1,3 @@
-import { Button } from "@/components/ui/button"
-import {
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow
-} from "@/components/ui/table";
 
 
 
