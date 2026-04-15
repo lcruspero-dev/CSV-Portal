0f97459a921e4bc24471e7c64ca783dbd2fbcd4c
lcruspero-dev <lcruspero@csvnow.com>
2026-04-15 23:57:13 +0800

    feat: Implement automatically mark as present in shiftAttendance

--- CSV-ticketing/src/API/endpoint.tsx
+++ CSV-ticketing/src/API/endpoint.tsx
@@ -210,7 +210,7 @@ export const ScheduleAndAttendanceAPI = {
       "POST",
       body
     ),
-  updateAttendanceEntry: (id: string, body: object) =>
+  updateAttendanceEntry: (id: string, formattedDate: string, body: object) =>
     apiHelper(
       `/api/ScheduleAndAttendanceRoutes/attendance-entries/${id}`,
       "PUT",
