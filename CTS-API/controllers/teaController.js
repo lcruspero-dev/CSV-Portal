8c3d3cdee432e5118afe0cc1c76e74376db5a20f
leescifer <leester9103@gmail.com>
2026-06-18 03:54:56 +0800

    int: Initial controller

--- CTS-API/controllers/teaController.js
+++ CTS-API/controllers/teaController.js
@@ -1,11 +1,35 @@
 const Tea = require("../models/teaModel");
 const mongoose = require("mongoose");
-const asyncHandler = require("express-async-handler");
 
-const canUpdateTea = (user) => {
-  return user.isAdmin || user.role === "TL" || user.role === "TM";
+const index = async (req, res) => {
+  try {
+    console.log("Perfect");
+  } catch (error) {
+    console.error("Failed to fetch data", error);
+    res.status(500).json({
+      status: "Error",
+      message: "Internal Server Error",
+    });
+  }
 };
 
-const get = asyncHandler(async (req, res) => {
-  const tea = await Tea.find();
-});
+const get = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    console.log("perfect");
+  } catch (error) {
+    console.error("Failed", error);
+    res.status(500).json({
+      status: "Error",
+      message: "Internal Server Error",
+    });
+  }
+};
+
+const create = async (req, res) => {};
+
+module.exports = {
+  index,
+  get,
+};
