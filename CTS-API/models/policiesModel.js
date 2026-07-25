dbebf5162d46613e8654a87d7862ba6611a0f5ac
leescifer <leester9103@gmail.com>
2026-07-26 01:07:55 +0800

    fix: add a isActive on each memo where if false that policies  will be hidden and only active shows in the employee portal

--- CTS-API/models/policiesModel.js
+++ CTS-API/models/policiesModel.js
@@ -1,39 +1,44 @@
 const mongoose = require("mongoose");
 
-const policiesSchema = mongoose.Schema({
-  subject: {
-    type: String,
-    required: [true, "Please add a subject"],
-  },
-  file: {
-    type: String,
-  },
-  description: {
-    type: String,
-    required: [true, "Please add a subject"],
-  },
-  acknowledgedby: [
-    {
-      name: {
-        type: String,
-      },
-      userId: {
-        type: String,
-      },
-      acknowledgedAt: {
-        type: Date,
-        default: Date.now,
-      },
+const policiesSchema = mongoose.Schema(
+  {
+    subject: {
+      type: String,
+      required: [true, "Please add a subject"],
     },
-  ],
-  isPinned: {
-    type: Boolean,
-    default: false,
+    file: {
+      type: String,
+    },
+    description: {
+      type: String,
+      required: [true, "Please add a subject"],
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    acknowledgedby: [
+      {
+        name: {
+          type: String,
+        },
+        userId: {
+          type: String,
+        },
+        acknowledgedAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+    isPinned: {
+      type: Boolean,
+      default: false,
+    },
   },
-},
-{
+  {
     timestamps: true,
-}
+  },
 );
 
 module.exports = mongoose.model("Policies", policiesSchema);
